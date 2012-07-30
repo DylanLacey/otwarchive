8e022f4b03eb758b62324dcea6f8b6d99230e744
Scott D. Sullivan <scottsds@gmail.com>
2012-07-30 02:15:56 -0400

    Added Screencasts

--- db/migrate/20120703032228_create_questions.rb
+++ db/migrate/20120703032228_create_questions.rb
@@ -3,11 +3,15 @@ class CreateQuestions < ActiveRecord::Migration
     create_table :questions do |t|
       t.integer :archive_faq_id
       t.string :question
-      t.text :answer
+      t.text :content
       t.string :anchor
-
+      t.text :screencast
       t.timestamps
     end
+    add_column :questions, :content_sanitizer_version, :integer, :default => 0, :null => false, :limit => 2
+    add_column :questions, :screencast_sanitizer_version, :integer, :default => 0, :null => false, :limit => 2
+    #remove_column :archive_faqs, :content_sanitizer_version
+    #remove_column :archive_faqs, :content
   end
 
   def self.down
